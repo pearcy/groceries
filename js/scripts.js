e5d4d30ac863078234b386dc3e1f2da669cbc6af
Joseph Pearce <joseph.pearce05@gmail.com>
2020-06-08 15:15:01 -0700

    add jQuery to generate list on front end

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,23 @@
 $(document).ready(function() {
   $("#form").submit(function(event) {
     event.preventDefault();
+    let item1 = $('input#item1').val();
+    let item2 = $('input#item2').val();
+    let item3 = $('input#item3').val();
+
+    console.log(item1);
+    console.log(item2);
+    console.log(item3);
+
+    let groceries = [item1, item2, item3];
+
+    groceries.forEach(function(grocery) {
+      $('.output').append('<ul>' + grocery + '</ul>');
+      console.log(grocery);
+    }) 
+
+
+
 
 
 
